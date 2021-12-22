b127e7302de96d72b7ff29e4b6c5c7b95e96eb9d
wanxwang <wanxin-wang@uiowa.edu>
2021-12-21 19:32:59 -0600

    initial

---
+++ inc/ik.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "pch.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+VRLOGLIB(float, ik_test)(float theta);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ inc/pch.h
@@ -0,0 +1,29 @@
+// pch.h: This is a precompiled header file.
+// Files listed below are compiled only once, improving build performance for future builds.
+// This also affects IntelliSense performance, including code completion and many code browsing features.
+// However, files listed here are ALL re-compiled if any one of them is updated between builds.
+// Do not add files here that you will be updating frequently as this negates the performance advantage.
+
+#ifndef PCH_H
+#define PCH_H
+
+// add headers that you want to pre-compile here
+#if defined _WINDOWS
+#	define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files
+#	include <windows.h>
+#endif
+
+// DLL export C functions:
+//		https://docs.microsoft.com/en-us/cpp/build/exporting-from-a-dll-using-declspec-dllexport?view=msvc-160
+// calling convention:
+//		https://en.wikipedia.org/wiki/X86_calling_conventions#Microsoft_x64_calling_convention
+#ifdef LIB_VRLOG_EXPORTS
+#define VRLOGLIB(rtype, func)\
+	__declspec(dllexport) rtype __stdcall func
+#else
+#define VRLOGLIB(rtype, func)\
+	__declspec(dllimport) rtype __stdcall func
+#endif
+
+#endif //PCH_H
